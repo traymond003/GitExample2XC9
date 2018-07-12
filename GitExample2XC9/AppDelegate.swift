7516ab356b181de3c2293cd4600d75c2f74ec4a6
Ted Raymond <traymond003@gmail.com>
2018-07-12 13:25:05 -0400

    Initial Commit

---
+++ GitExample2XC9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExample2XC9
+//
+//  Created by Theodore Raymond on 7/12/18.
+//  Copyright © 2018 Bethany Raymond. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitExample2XC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExample2XC9
+//
+//  Created by Theodore Raymond on 7/12/18.
+//  Copyright © 2018 Bethany Raymond. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
